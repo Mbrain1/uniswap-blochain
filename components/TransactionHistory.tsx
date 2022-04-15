7bc6ca7f83d0c8f0548d22fa71ab9f4cc39179aa
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2022-04-15 08:42:10 +0100

    Update

--- components/TransactionHistory.tsx
+++ components/TransactionHistory.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const TransactionHistory = ({ children } : any ) => {
 	const { amount, created_at, to, trxnHash } = children;
-	const time = created_at.seconds.toDate();
+	const time = "Fri 24, 2022 by 10AM"; //created_at.seconds.toDate();
 	return (
 		<div className={`rounded-xl px-4 py-2 text-white bg-[#191a1e] my-2 text-sm`}>
 		    <span>{amount} ETH sent to {(to).slice(0, 7)}... on {time}</span>  <a target="_blank" href={`https://rinkeby.etherscan.io/tx/`+trxnHash} className={`text-blue-500`}>View on Etherscan</a>
